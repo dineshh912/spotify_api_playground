38abf2eb16ab4177426302c0ab547bccc9895aad
dineshh912 <dineshh912@gmail.com>
2022-04-26 09:32:19 +0100

    added-remaining-endpoints

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import sample_ids
 from pprint import pprint
 
 """
-note: To access user related api endpoints,
-      need to use different auth method than client_credentials
+note: To access user related api endpoints, 
+    (the endpoints start with me/ and post, put, delete)
+    need to use different auth method than client_credentials
+    so, it's not covered in this code base
 """
 
 # Authentication
@@ -156,7 +158,7 @@ def get_related_artists(artist_id):
 
 ########## TRACK ######################################
 
-def get_track_info(track_id):
+def get_track(track_id):
     """
         Get the track information using track id
         input: track_id: track id (string)
@@ -164,7 +166,104 @@ def get_track_info(track_id):
     track_res = requests.get(f'{config.BASE_URL}/tracks/{track_id}',
                              headers=headers)
     return track_res.json()
-  
+
+
+def get_tracks(track_ids):
+    """
+        Get the track information using track id
+        input: track_ids: list of track ids (string)
+    """
+    tracks_res = requests.get(f'{config.BASE_URL}/tracks?ids={track_ids}',
+                              headers=headers)
+    return tracks_res.json()
+
+
+def get_track_audio_feature(track_id):
+    """
+        Get audio features of the track using track id
+        input: track_id: track id (string)
+    """
+    track_audio_feature_res = requests.get(f'{config.BASE_URL}/audio-features/{track_id}',
+                                           headers=headers)
+    return track_audio_feature_res.json()
+
+
+def get_track_audio_features(track_ids):
+    """
+        Get audio features for a track
+        input: track_ids: list of track ids (string)
+        output: list of track objects
+    """
+    track_audio_features_res = requests.get(f'{config.BASE_URL}/audio-features?ids={track_ids}',
+                                            headers=headers)
+    return track_audio_features_res.json()
+
+
+def get_audio_analysis(track_id):
+    """
+        Get audio analysis of the track using track id
+        input: track_id: track id (string)
+    """
+    audio_analysis_res = requests.get(f'{config.BASE_URL}/audio-analysis/{track_id}',
+                                      headers=headers)
+    return audio_analysis_res.json()
+
+
+def get_track_recommendations(**kwargs):
+    """
+        Get recommendations for a track
+        output: list of track objects
+        Query Parameters: seed_artist: list of artist ids (string) (0-5)
+                         seed_genres: list of genre (string) (0-5)
+                         seed_tracks: list of track ids (string) (0-5)
+                         limit: integer (0-100) default: 20
+                         market: Country: ISO 3166-1 alpha-2 country code.
+                         max_acousticness: float (0.0-1.0)
+                         max_danceability: float (0.0-1.0)
+                         max_duration_ms: integer
+                         max_energy: float (0.0-1.0)
+                         max_instrumentalness: float (0.0-1.0)
+                         max_key: integer (0-11)
+                         max_liveness: float (0.0-1.0)
+                         max_loudness: float (-60.0-0.0)
+                         max_mode: integer (0-11)
+                         max_popularity: integer (0-100)
+                         max_speechiness: float (0.0-1.0)
+                         max_tempo: integer (0-500)
+                         max_time_signature: integer (0-11)
+                         max_valence: float (0.0-1.0)
+                         min_acousticness: float (0.0-1.0)
+                         min_danceability: float (0.0-1.0)
+                         min_duration_ms: integer
+                         min_energy: float (0.0-1.0)
+                         min_instrumentalness: float (0.0-1.0)
+                         min_key: integer (0-11)
+                         min_liveness: float (0.0-1.0)
+                         min_loudness: float (-60.0-0.0)
+                         min_mode: integer (0-11)
+                         min_popularity: integer (0-100)
+                         min_speechiness: float (0.0-1.0)
+                         min_tempo: integer (0-500)
+                         min_time_signature: integer (0-11)
+                         min_valence: float (0.0-1.0)
+                         target_acousticness: float (0.0-1.0)
+                         target_danceability: float (0.0-1.0)
+                         target_duration_ms: integer
+                         target_energy: float (0.0-1.0)
+                         target_instrumentalness: float (0.0-1.0)
+                         target_key: integer (0-11)
+                         target_liveness: float (0.0-1.0)
+                         target_loudness: float (-60.0-0.0)
+                         target_mode: integer (0-11)
+                         target_popularity: integer (0-100)
+                         target_speechiness: float (0.0-1.0)
+                         target_tempo: integer (0-500)
+                         target_time_signature: integer (0-11)
+                         target_valence: float (0.0-1.0)
+    """
+    track_recommendations_res = requests.get(f'{config.BASE_URL}/recommendations',
+                                             headers=headers, params=kwargs)
+    return track_recommendations_res.json()
     
 ########## SHOWS ######################################
 
@@ -204,3 +303,188 @@ def get_show_episodes(show_id, market):
     show_episodes_res = requests.get(f'{config.BASE_URL}/shows/{show_id}/episodes?market={market}',
                                      headers=headers)
     return show_episodes_res.json()
+
+
+########## Episodes ######################################
+
+def get_episode(episode_id, market):
+    """
+        Get the episode information using episode id
+        input: episode_id: episode id (string)
+        output: episode object
+        Query Parameters: market: Country: ISO 3166-1 alpha-2 country code.(required)
+    """
+    episode_res = requests.get(f'{config.BASE_URL}/episodes/{episode_id}?market={market}',
+                               headers=headers)
+    return episode_res.json()
+
+
+def get_episodes(episode_ids, market):
+    """
+        Get multiple episode information using episode id
+        input: episode_ids: list of episode ids (string)
+        output: list of episode objects
+        Query Parameters: market: Country: ISO 3166-1 alpha-2 country code.(required)
+    """
+    episodes_res = requests.get(f'{config.BASE_URL}/episodes?ids={episode_ids}&market={market}',
+                                headers=headers)
+    return episodes_res.json()
+
+
+########## Search ######################################
+
+def search(**kwargs):
+    """
+        Search for an item
+        input: q: search query (string) required
+               type: type of search (string)
+               include_external: include external search results (boolean)
+               market: Country: ISO 3166-1 alpha-2 country code.(required)
+               limit: integer (0-50) default: 20
+               offset: integer  default: 0
+        output: list of search results
+    """
+    search_res = requests.get(f'{config.BASE_URL}/search',params=kwargs,
+                              headers=headers)
+    return search_res.json()
+
+########## Users ######################################
+
+def get_user_profile(user_id):
+    """
+        Get the user profile information using user id
+        input: user_id: user id (string)
+        output: user object
+    """
+    user_profile_res = requests.get(f'{config.BASE_URL}/users/{user_id}',
+                                    headers=headers)
+    return user_profile_res.json()
+
+
+########## Playlist ######################################
+
+def get_playlist(playlist_id, **kwargs):
+    """
+        Get the playlist information using playlist id
+        input: playlist_id: playlist id (string)
+        output: playlist object
+        Query Parameters: market: Country: ISO 3166-1 alpha-2 country code.
+                                  additional_types
+                                  fields
+    """
+    playlist_res = requests.get(f'{config.BASE_URL}/playlists/{playlist_id}',
+                                params=kwargs, headers=headers)
+    return playlist_res.json()
+
+
+def get_playlist_items(playlist_id, **kwargs):
+    """
+        Get the playlist items using playlist id
+        input: playlist_id: playlist id (string)
+        output: list of playlist item objects
+        Query Parameters: market: Country: ISO 3166-1 alpha-2 country code.
+                                  additional_types
+                                  fields
+                                  limit: integer (0-50) default: 20
+                                  offset: integer  default: 0
+    """
+    playlist_items_res = requests.get(f'{config.BASE_URL}/playlists/{playlist_id}/tracks',
+                                      params=kwargs, headers=headers)
+    return playlist_items_res.json()
+
+
+def get_user_playlist(user_id, **kwargs):
+    """
+        Get the user playlists using user id
+        input: user_id: user id (string)
+        output: list of playlist objects
+        Query Parameters:   limit: integer (0-50) default: 20
+                            offset: integer  default: 0
+    """
+    user_playlist_res = requests.get(f'{config.BASE_URL}/users/{user_id}/playlists',
+                                     params=kwargs, headers=headers)
+    return user_playlist_res.json()
+
+
+def get_featured_playlists(**kwargs):
+    """
+        Get the featured playlists
+        input: None
+        output: list of playlist objects
+        Query Parameters: country: Country: ISO 3166-1 alpha-2 country code.
+                                  locale
+                                  timestamp
+                                  limit: integer (0-50) default: 20
+                                  offset: integer  default: 0
+    """
+    featured_playlists_res = requests.get(f'{config.BASE_URL}/browse/featured-playlists',
+                                          params=kwargs, headers=headers)
+    return featured_playlists_res.json()
+
+
+def get_category_playlists(category_id, **kwargs):
+    """
+        Get the category playlists using category id
+        input: category_id: category id (string)
+        output: list of playlist objects
+        Query Parameters: country: Country: ISO 3166-1 alpha-2 country code.
+                                  limit: integer (0-50) default: 20
+                                  offset: integer  default: 0
+    """
+    category_playlists_res = requests.get(f'{config.BASE_URL}/browse/categories/{category_id}/playlists',
+                                          params=kwargs, headers=headers)
+    return category_playlists_res.json()
+
+
+def get_playlist_cover_image(playlist_id):
+    """
+        Get the playlist cover image using playlist id
+        input: playlist_id: playlist id (string)
+        output: image url
+    """
+    playlist_cover_image_res = requests.get(f'{config.BASE_URL}/playlists/{playlist_id}/images',
+                                            headers=headers)
+    return playlist_cover_image_res.json()
+
+
+########## Categories ######################################
+
+def get_categories(**kwargs):
+    """
+        Get the categories
+        input: None
+        output: list of category objects
+        Query Parameters: country: Country: ISO 3166-1 alpha-2 country code.
+                                  locale
+                                  limit: integer (0-50) default: 20
+                                  offset: integer  default: 0
+    """
+    categories_res = requests.get(f'{config.BASE_URL}/browse/categories',
+                                  params=kwargs, headers=headers)
+    return categories_res.json()
+
+
+def get_single_category(category_id):
+    """
+        Get the category using category id
+        input: category_id: category id (string)
+        output: category object
+    """
+    single_category_res = requests.get(f'{config.BASE_URL}/browse/categories/{category_id}',
+                                       headers=headers)
+    return single_category_res.json()
+
+
+########## Genres ######################################
+
+def get_available_genre_seeds(**kwargs):
+    """
+        Get the available genre seeds
+        input: None
+        output: list of genre objects
+        Query Parameters: country: Country: ISO 3166-1 alpha-2 country code.
+                                  locale
+    """
+    available_genre_seeds_res = requests.get(f'{config.BASE_URL}/recommendations/available-genre-seeds',
+                                             params=kwargs, headers=headers)
+    return available_genre_seeds_res.json()
